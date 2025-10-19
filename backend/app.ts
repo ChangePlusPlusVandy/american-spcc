d29ce2301f06986df925045362f0e886126a172a
sophie-26 <sophiezhuang06@gmail.com>
2025-10-19 10:33:52 -0500

    delet#

--- backend/app.ts
+++ backend/app.ts
@@ -1,16 +1,11 @@
 import express from 'express';
 import cors from 'cors';
-import resourceRoutes from './routes/resourcesRoutes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Mount resource routes
-app.use('/api/resources', resourceRoutes);
-console.log('✅ Resource routes mounted at /api/resources');
-
-app.get('/', (req, res) => res.send('API running ✅'));
+app.get('/', (req, res) => res.send('API running'));
 
 export default app;
