f8c45602631e51aecc30470f602200154212360d
sophie-26 <sophiezhuang06@gmail.com>
2025-10-28 15:48:30 -0500

    frontend additions

---
+++ frontend/src/pages/LandingPage/Landing.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
+
+function Landing() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Landing Page</h1>
+      <SignedOut>
+        <p>You are signed out</p>
+        <Link to="/sign-in">Sign In</Link>
+      </SignedOut>
+      <SignedIn>
+        <p>You are signed in!</p>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
+}
+
+export default Landing;
---
+++ frontend/src/pages/LoginPage/Login.tsx
@@ -0,0 +1,15 @@
+import { SignIn } from "@clerk/clerk-react";
+
+function Login() {
+  return (
+    <div style={{ marginTop: "5rem", display: "flex", justifyContent: "center" }}>
+      <SignIn 
+        routing="path" 
+        path="/sign-in"
+        signUpUrl="/sign-up"
+      />
+    </div>
+  );
+}
+
+export default Login;
