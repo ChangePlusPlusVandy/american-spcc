eab196ff6004f702df75005c643c2ebe1240d224
JPark1023 <jaimespark@gmail.com>
2025-11-10 15:13:27 -0600

    Update NavBar for clerk and final design
    This commit updates the navbar to match the figma final design and
    integrates clerk: IF the user is signed out, there is a sign in button
    at the top of the page redirecting to the sign-in page. Otherwise, it
    displays the userprofile which on click will redirect to the account
    page. This uses clerk's built in UserButton which pre-populates an
    account page.

--- frontend/src/components/NavBarComponent/NavBar.tsx
+++ frontend/src/components/NavBarComponent/NavBar.tsx
@@ -4,32 +4,56 @@ import navSidebar from '../../assets/nav_sidebar_button.png';
 import donateButton from '../../assets/donate_button.png';
 import aspccLogoTurqoise from '../../assets/aspcc_logo_turqois.png';
 import chevron from '../../assets/chevron.png';
+import { useNavigate } from 'react-router-dom';
+
+import { SignedIn, SignedOut, UserButton } from '@clerk/clerk-react';
 
 export default function DefaultNav() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSignInClick = () => {
+    navigate('/sign-in');
+  };
 
   return (
     <nav
-      className={'relative z-50 w-full bg-[#FFF9F0] px-6 py-4 flex items-center justify-between'}
+      className={'relative z-50 w-full bg-[#FFF9F0] px-4 py-4 flex items-center justify-between'}
     >
-      {/* Left: Hamburger */}
-      <button
-        style={{
-          background: 'transparent',
-          border: 'none',
-          padding: 0,
-          margin: 0,
-          boxShadow: 'none',
-          appearance: 'none',
-          outline: 'none',
-        }}
-        onClick={() => setMenuOpen(!menuOpen)}
-      >
-        <img src={navSidebar} alt="Menu" className="relative left-7 h-5 w-5" />
-      </button>
+      <div className="flex items-center gap-x-10">
+        {/* Left: Hamburger */}
+        <button
+          style={{
+            background: 'transparent',
+            border: 'none',
+            padding: 0,
+            margin: 0,
+            boxShadow: 'none',
+            appearance: 'none',
+            outline: 'none',
+          }}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <img src={navSidebar} alt="Menu" className="relative left-4 h-5 w-5" />
+        </button>
+        <button
+          style={{
+            background: 'transparent',
+            border: 'none',
+            padding: 0,
+            margin: 0,
+            boxShadow: 'none',
+            appearance: 'none',
+            outline: 'none',
+          }}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <img src={aspccLogoTurqoise} alt="logo" className="relative h-10" />
+        </button>
+      </div>
 
       {/* Center: Navigation Links */}
-      <div className="flex items-center gap-x-8 font-semibold text-sm">
+      <div className="flex items-center gap-x-6 font-semibold text-sm">
         <a href="#" className="!text-[#566273] hover:text-[#566273]">
           Programs
         </a>
@@ -48,7 +72,7 @@ export default function DefaultNav() {
           >
             About <img src={chevron} alt="" className="ml-1 mt-1 h-3 w-3 inline-block" />
           </button>
-          {/* Dropdown example */}
+          {/* Dropdown*/}
           <div className="absolute hidden group-hover:block bg-white shadow-md rounded-md mt-2">
             <a href="#" className="block px-4 py-2 !text-[#566273] hover:text-[#566273]">
               Example 1
@@ -100,8 +124,9 @@ export default function DefaultNav() {
             <img src={donateButton} alt="donate" className="relative h-7" />
           </button>
         </div>
-        <div className="flex items-center">
+        <SignedOut>
           <button
+            className="flex items-center !text-[#566273] hover:text-[#566273]"
             style={{
               background: 'transparent',
               border: 'none',
@@ -111,11 +136,14 @@ export default function DefaultNav() {
               appearance: 'none',
               outline: 'none',
             }}
-            onClick={() => setMenuOpen(!menuOpen)}
+            onClick={() => handleSignInClick()}
           >
-            <img src={aspccLogoTurqoise} alt="logo" className="relative h-10" />
+            Sign-In
           </button>
-        </div>
+        </SignedOut>
+        <SignedIn>
+          <UserButton userProfileUrl="/account"></UserButton>
+        </SignedIn>
       </div>
     </nav>
   );
