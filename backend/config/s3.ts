96d8e097d98e68f6f8df182140c945fc38651426
sophie-26 <sophiezhuang06@gmail.com>
2026-01-06 22:57:17 -0600

    s3

--- backend/config/s3.ts
+++ backend/config/s3.ts
@@ -1,7 +1,4 @@
-import { S3Client } from '@aws-sdk/client-s3';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import { S3Client } from '@aws-sdk/client-s3'
 
 export const s3 = new S3Client({
   region: process.env.AWS_REGION || 'us-east-2',
@@ -9,4 +6,4 @@ export const s3 = new S3Client({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
-});
+})
