84d522eeee3f6472c367f72e917a73233d1bb7ba
sophie-26 <sophiezhuang06@gmail.com>
2026-01-06 23:00:39 -0600

    fix prisma

--- backend/config/prisma.ts
+++ backend/config/prisma.ts
@@ -1,3 +1,23 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-export default prisma;
+import { PrismaClient } from '@prisma/client'
+
+const globalPrisma = global as unknown as { prisma: PrismaClient }
+
+export const prisma =
+  globalPrisma.prisma ||
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+    log:
+      process.env.NODE_ENV === 'development'
+        ? ['query', 'error', 'warn']
+        : ['error'],
+  })
+
+if (process.env.NODE_ENV !== 'production') {
+  globalPrisma.prisma = prisma
+}
+
+export default prisma
