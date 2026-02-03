c4f62f69d86bfb2f47c7049c868440c15ac5a22e
sophie-26 <sophiezhuang06@gmail.com>
2026-02-03 10:34:58 -0600

    export server

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,7 @@
 import app from './app';
 
 //local dev only - replace "export default app;" with:
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+//const PORT = process.env.PORT || 8000;
+//app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
-//export default app;
+export default app;
