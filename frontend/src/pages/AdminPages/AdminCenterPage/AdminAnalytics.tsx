546c935608b2fa292cba7812472cf800e6657dee
sophie-26 <sophiezhuang06@gmail.com>
2026-02-16 10:03:11 -0600

    analytics added

--- frontend/src/pages/AdminPages/AdminCenterPage/AdminAnalytics.tsx
+++ frontend/src/pages/AdminPages/AdminCenterPage/AdminAnalytics.tsx
@@ -1,4 +1,22 @@
 import './AdminCenter.css';
+
 export default function AdminAnalytics() {
-  return <h3 className="admin-panel-title">Data Analytics</h3>;
+  return (
+    <div className="admin-analytics-container">
+      <h3 className="admin-panel-title">Data Analytics</h3>
+
+      <div className="analytics-iframe-wrapper">
+      <iframe
+        width="600"
+        height="443"
+        src="https://lookerstudio.google.com/embed/reporting/5df1bdee-39f0-46eb-82fa-9ff8becf729b/page/aMnoF"
+        frameBorder="0"
+        style={{ border: 0 }}
+        allowFullScreen
+        sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
+      />
+      </div>
+    </div>
+  );
 }
+
