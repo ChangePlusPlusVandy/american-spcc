b60fef31b083051fc12419b017f7ca51b2ddda3a
sophie-26 <sophiezhuang06@gmail.com>
2026-02-16 10:18:15 -0600

    css analytics

--- frontend/src/pages/AdminPages/AdminCenterPage/AdminAnalytics.tsx
+++ frontend/src/pages/AdminPages/AdminCenterPage/AdminAnalytics.tsx
@@ -7,14 +7,13 @@ export default function AdminAnalytics() {
 
       <div className="analytics-iframe-wrapper">
       <iframe
-        width="600"
-        height="443"
         src="https://lookerstudio.google.com/embed/reporting/5df1bdee-39f0-46eb-82fa-9ff8becf729b/page/aMnoF"
         frameBorder="0"
         style={{ border: 0 }}
         allowFullScreen
         sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
       />
+
       </div>
     </div>
   );
